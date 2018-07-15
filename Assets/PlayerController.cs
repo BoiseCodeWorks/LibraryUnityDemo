2fb946b9204007a04a444d7f327fab6303589c67
Jake Overall <joverall22@gmail.com>
2018-07-15 16:09:00 -0600

    end day-2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     public float WalkSpeed = 5;
     public float RunSpeed = 10;
     public float JumpForce = 5;
-
+    public Camera CamTransform;
 
     // Use this for initialization
     void Start()
@@ -26,6 +26,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GetPlayerInput();
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("StartAttack");
+        }
     }
 
     void GetPlayerInput()
@@ -35,15 +39,41 @@ public class PlayerController : MonoBehaviour
         dir.z = Input.GetAxis("Vertical");
         var walking = Input.GetKey(KeyCode.LeftShift);
 
+        if(dir.z != 0)
+        {
+            if (walking)
+            {
+                anim.SetFloat("Speed", .3f);
+            }else
+            {
+                anim.SetFloat("Speed", 1);
+            }
+        }else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
         if (dir.magnitude > 1)
         {
             dir.Normalize();
         }
 
+        dir = RotateWithView(dir);
+
         controller.Move(
             dir * Time.deltaTime * 
             (walking ? WalkSpeed : RunSpeed) 
             );
+    }
+
+    private Vector3 RotateWithView(Vector3 input)
+    {
+        Vector3 dir = CamTransform.transform.TransformDirection(input);
+        dir.Set(dir.x, 0, dir.z);
+        float yAngle = (CamTransform.transform.rotation.y / 180) * 100;
+        //Debug.Log(yAngle);
+        transform.localRotation = new Quaternion(0, yAngle, 0, 0);
+        return dir.normalized * input.magnitude;
     }
 
 }
